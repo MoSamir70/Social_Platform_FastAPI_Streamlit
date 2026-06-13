24fb08165d751913be75caf4081728bfc386af18
MoSamir70 <mosamirnozom@gmail.com>
2026-06-13 08:35:20 +0300

    - Retreving data from DB - Uplaod & Feed Endpoints - Upload images &Videos

--- app/app.py
+++ app/app.py
@@ -1,9 +1,22 @@
-from fastapi import FastAPI, HTTPException
+#from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, File, UploadFile, Form, Depends
 from app.schemas import PostCreate , PostResponse
+
 
 from app.db import Post, create_db_and_tables, get_async_session
 from sqlalchemy.ext.asyncio import AsyncSession
 from contextlib import asynccontextmanager
+
+from sqlalchemy import select
+from app.images import imagekit
+from imagekitio.models.UploadFileRequestOptions import UploadFileRequestOptions
+
+import shutil
+import os
+import uuid
+import tempfile
+
+#from app.users import auth_backend, current_active_user, fastapi_users
 
 
 @asynccontextmanager
@@ -78,4 +91,82 @@ app = FastAPI(lifespan=lifespan)
 
 #--------------------------------------- Database Connection 46:05
 #----- Create Upload + Save Database 59:05
- 
+
+
+@app.post("/upload")
+async def upload_file(
+        file: UploadFile = File(...),
+        caption: str = Form(""),
+     #   user: User = Depends(current_active_user),
+        session: AsyncSession = Depends(get_async_session) # Dependency Injection
+):
+    temp_file_path = None
+
+    try:
+        with tempfile.NamedTemporaryFile(delete=False, suffix=os.path.splitext(file.filename)[1]) as temp_file:
+            temp_file_path = temp_file.name
+            shutil.copyfileobj(file.file, temp_file) # copy from file.file to temp_file
+
+       # upload file to imagekit
+        upload_result = imagekit.upload_file(
+            file=open(temp_file_path, "rb"), # rb = read bytes
+            file_name=file.filename,
+            options=UploadFileRequestOptions(
+                use_unique_file_name=True,
+                tags=["backend-upload"]
+            )
+        )
+
+        if upload_result.response_metadata.http_status_code == 200:
+            post = Post(
+              #  user_id=user.id,
+                caption=caption,
+                url=upload_result.url,
+                file_type="video" if file.content_type.startswith("video/") else "image",
+                file_name=upload_result.name
+            )
+            session.add(post)
+            await session.commit() # post = create -> add = add -> commit = write in db
+            await session.refresh(post)
+            return post
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    finally:
+        if temp_file_path and os.path.exists(temp_file_path):
+            os.unlink(temp_file_path)
+        file.file.close()
+
+
+#---------------- Retrieving Form DB -> 01:05:00
+
+@app.get("/feed")
+async def get_feed(
+        session: AsyncSession = Depends(get_async_session),
+      #  user: User = Depends(current_active_user),
+):
+    result = await session.execute(select(Post).order_by(Post.created_at.desc()))
+    posts = [row[0] for row in result.all()]  # cursor object
+
+    #result = await session.execute(select(User))
+    users = [row[0] for row in result.all()]
+    user_dict = {u.id: u.email for u in users}
+
+    posts_data = []
+    for post in posts:
+        posts_data.append(
+            {
+                "id": str(post.id),
+                "user_id": str(post.user_id),
+                "caption": post.caption,
+                "url": post.url,
+                "file_type": post.file_type,
+                "file_name": post.file_name,
+                "created_at": post.created_at.isoformat(),
+             #   "is_owner": post.user_id == user.id,
+                "email": user_dict.get(post.user_id, "Unknown")
+            }
+        )
+
+    return {"posts": posts_data}
+
